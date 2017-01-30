1a2ded29fb757bb41310e2d7347c4e4d17182fbf
Rohit Suratekar <rohitsuratekar@gmail.com>
2017-01-30 19:10:03 +0530

    Initial commit

---
+++ secrets/credentials.py
@@ -0,0 +1,5 @@
+"""
+All project related credentials will come into this file
+This file will be untracked.
+e.g.: api key, database urls etc
+"""
---
+++ user_data/README.md
@@ -0,0 +1,1 @@
+This package will contain all user data. This package will be untracked for security purpose.
